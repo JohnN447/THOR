0d08dedb85a284190cff63a34da70222ff7c418d
Saint <saint.github@outlook.at>
2015-03-12 17:24:29 +0100

    Arma 3 Modstructure
    selfexplaning

---
+++ THOR Megapack/mod.cpp
@@ -0,0 +1,6 @@
+name = "THOR Megapack";
+description = "Mods created by Members of Team THOR for Team THOR and others";
+actionName = "Team Thor Website";
+action = "http://team-thor.de/";
+
+author = "Team THOR";
