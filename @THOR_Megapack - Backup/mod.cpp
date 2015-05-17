d576d64e06442fef9fc4aebdd67b7e945256f3e6
John Doe <Spycopter123@web.de>
2015-05-17 14:21:15 +0200

    Created Backup of working model, changed existing model
    Fixed a bug rendering the shadow of the PDW in wrong depth.

--- @THOR_Megapack - Backup/mod.cpp
+++
@@ -1,6 +0,0 @@
-name = "THOR Megapack";
-description = "Mods created by Members of Team THOR for Team THOR and others";
-actionName = "Team Thor Website";
-action = "http://team-thor.de/";
-
-author = "Team THOR";
--- @THOR_Megapack - Backup/readme.txt
+++
@@ -1,3 +0,0 @@
-
-
-textures created using brushes made by Panzerfire (www.panzerfire.deviantart.com)
