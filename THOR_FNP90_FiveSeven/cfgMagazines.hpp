747c7886b95bd31766349b88a4cf951bed6e8239
Saint <saint.github@outlook.at>
2015-05-17 14:33:34 +0200

    fixed localization
    $str is not allowed to be in ""

--- THOR_FNP90_FiveSeven/cfgMagazines.hpp
+++ THOR_FNP90_FiveSeven/cfgMagazines.hpp
@@ -2,7 +2,7 @@ class CA_Magazine;
 class THOR_FNP90_Mag: CA_Magazine
 	{
 		scope=2;
-		displayName="$STR_THOR_FNP90_Mag";
+		displayName=$STR_THOR_FNP90_Mag;
 		picture="\A3\weapons_f\data\ui\M_30Rnd_9x21_CA.paa";
         model="\THOR_FNP90_FiveSeven\data\THOR_FNP90_Mag.p3d";
 		ammo="THOR_57x28_Ball";
@@ -10,12 +10,12 @@ class THOR_FNP90_Mag: CA_Magazine
 		mass=14;
 		initSpeed=370;
 		tracersEvery=0;
-		descriptionShort="$STR_THOR_FNP90_Mag_Desc";
+		descriptionShort=$STR_THOR_FNP90_Mag_Desc;
 	};
   
 class THOR_FNP90_Mag_Tracer : THOR_FNP90_Mag
 	{
-		displayName="$STR_THOR_FNP90_Mag_Tracer";
+		displayName=$STR_THOR_FNP90_Mag_Tracer;
 		picture="\A3\weapons_f\data\ui\M_30Rnd_9x21_CA.paa";
         model="\THOR_FNP90_FiveSeven\data\THOR_FNP90_Mag.p3d";
 		ammo="THOR_57x28_Tracer_Red";
@@ -23,5 +23,5 @@ class THOR_FNP90_Mag_Tracer : THOR_FNP90_Mag
 		mass=14;
 		initSpeed=370;
 		tracersEvery=1;
-		descriptionShort="$STR_THOR_FNP90_Mag_Tracer_Desc"; 
+		descriptionShort=$STR_THOR_FNP90_Mag_Tracer_Desc; 
 	};
--- THOR_FNP90_FiveSeven/cfgWeapons.hpp
+++ THOR_FNP90_FiveSeven/cfgWeapons.hpp
@@ -20,7 +20,7 @@ class THOR_FNP90_base: Rifle_Base_F
     discreteDistanceInitIndex=0;
     aiDispersionCoefY=8;
     aiDispersionCoefX=9;
-    descriptionShort="$STR_THOR_FNP90_Desc";
+    descriptionShort=$STR_THOR_FNP90_Desc;
     handAnim[]=
     {
         "OFP2_ManSkeleton",
@@ -552,12 +552,12 @@ class THOR_FNP90: THOR_FNP90_base
     inertia=0.40000001;
     dexterity=1.6;
     initSpeed=370;
-    displayName="$STR_THOR_FNP90";
+    displayName=$STR_THOR_FNP90;
     picture="\THOR_FNP90_FiveSeven\data\icons\THOR_FNP90.paa";
     UiPicture="\THOR_FNP90_FiveSeven\data\icons\THOR_FNP90.paa";
     class Library
     {
-        libTextDesc="$STR_THOR_FNP90_Library";
+        libTextDesc=$STR_THOR_FNP90_Library;
     };
-    descriptionShort="$STR_THOR_FNP90_Desc";
+    descriptionShort=$STR_THOR_FNP90_Desc;
 };
